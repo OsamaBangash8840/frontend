0fd7b0c43c45347c0199488a14653658a138ea9b
OsamaBangash8840 <osama.khan8840@gmail.com>
2025-07-30 16:15:33 +0500

    changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NotificationsInitializer from "./components/NotificationsInitializer/NotificationsInitializer";
-import NotificationSetup from "./components/NotificationsInitializer/NotificationsSetup";
+// import NotificationsInitializer from "./components/NotificationsInitializer/NotificationsInitializer";
+// import NotificationSetup from "./components/NotificationsInitializer/NotificationsSetup";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NotificationSetup />
-        <NotificationsInitializer />
+        {/* <NotificationSetup />
+        <NotificationsInitializer /> */}
         {children}
       </body>
     </html>
--- src/app/utils/updateFcmToken.js
+++ src/app/utils/updateFcmToken.js
@@ -1,16 +1,16 @@
-import getFcmToken from "@/app/utils/getFcmToken";
-import apiClient from '../lib/api-client';
+// import getFcmToken from "@/app/utils/getFcmToken";
+// import apiClient from '../lib/api-client';
 
-const updateFcmToken = async () => {
-    const token = await getFcmToken();
+// const updateFcmToken = async () => {
+//     const token = await getFcmToken();
 
-    if (token) {
-        await apiClient.post(
-            "/v1/user/update-fcm-token",
-            { fcmToken: token },
-            { withCredentials: true }
-        );
-    }
-};
+//     if (token) {
+//         await apiClient.post(
+//             "/v1/user/update-fcm-token",
+//             { fcmToken: token },
+//             { withCredentials: true }
+//         );
+//     }
+// };
 
-export default updateFcmToken
+// export default updateFcmToken
